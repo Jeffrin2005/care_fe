e6702d04ee79ded84ea4e3de32abc5a9fda4c714
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-12-28 14:20:52 +0530

    latest-final

--- cypress/e2e/patient_spec/PatientInvestigation.cy.ts
+++ cypress/e2e/patient_spec/PatientInvestigation.cy.ts
@@ -8,49 +8,36 @@ describe("Patient Investigation Creation from Patient consultation page", () => 
   const patientPage = new PatientPage();
   const patientInvestigation = new PatientInvestigation();
   const patientName = "Dummy Patient Thirteen";
-
   before(() => {
     loginPage.loginByRole("districtAdmin");
     cy.saveLocalStorage();
   });
-
   beforeEach(() => {
     cy.restoreLocalStorage();
     cy.clearLocalStorage(/filters--.+/);
     cy.awaitUrl("/patients");
   });
-
   it("Create a investigation for a patient and verify its reflection", () => {
     patientPage.visitPatient(patientName);
     cy.url().should("include", "/patient");
 
-    cy.log("Clicking investigation tab");
     patientInvestigation.clickInvestigationTab();
-    cy.get("#consultation_tab_nav").should("be.visible").should("exist");
+    cy.get("#consultation_tab_nav").should("exist");
 
-    cy.log("Attempting to click log lab results");
-    cy.wait(2000);
     patientInvestigation.clickLogLabResults();
+    cy.get("#log-lab-results").should("exist");
 
-    cy.get("#log-lab-results", { timeout: 15000 })
-      .should("be.visible")
-      .should("exist");
-
-    cy.log("Selecting investigation options");
     patientInvestigation.selectInvestigationOption([
       "Haematology",
       "Urine Test",
     ]);
-
     cy.get("button")
       .contains("Save Investigation")
       .should("be.visible")
       .click();
-
     cy.verifyNotification("Please Enter at least one value");
     cy.closeNotification();
   });
-
   afterEach(() => {
     cy.saveLocalStorage();
   });
--- cypress/pageobject/Patient/PatientInvestigation.ts
+++ cypress/pageobject/Patient/PatientInvestigation.ts
@@ -1,126 +1,33 @@
 class PatientInvestigation {
-  private elements = {
-    addInvestigation: () => cy.get("#investigation"),
-    investigationTab: () => cy.get("#consultation_tab_nav"),
-    searchPatientInvestigation: () => cy.get("#search-patient-investigation"),
-    investigationGroup: () => cy.get("#investigation-group"),
-    investigationCheckbox: () => cy.get("#investigation-checkbox"),
-    investigationSelect: () => cy.get("#investigation-select"),
-    logLabResults: () => cy.get("#log-lab-results"),
-    investigationFrequency: () => cy.get("#investigation-frequency"),
-  };
-
   clickAddInvestigation() {
-    cy.log("Clicking Add Investigation");
-    this.elements
-      .addInvestigation()
-      .should("be.visible")
-      .contains("Add Investigation")
-      .click();
-    cy.wait(1000);
+    cy.verifyAndClickElement("#investigation", "Add Investigation");
   }
 
   clickInvestigationTab() {
-    cy.log("Clicking Investigation Tab");
-    this.elements
-      .investigationTab()
-      .should("be.visible")
-      .contains("Investigations")
-      .click();
-    cy.wait(2000);
+    cy.verifyAndClickElement("#consultation_tab_nav", "Investigations");
   }
 
   selectInvestigation(investigation: string) {
-    cy.log(`Selecting Investigation: ${investigation}`);
-
-    this.elements
-      .searchPatientInvestigation()
-      .should("be.visible")
-      .clear()
-      .type(investigation, { delay: 100 });
-
-    this.elements
-      .investigationGroup()
-      .contains(investigation)
-      .should("be.visible")
-      .click();
-
-    this.elements
-      .addInvestigation()
-      .contains("Investigation No. 1")
-      .should("be.visible")
-      .click();
+    cy.get("#search-patient-investigation").type(investigation);
+    cy.verifyAndClickElement("#investigation-group", investigation);
+    cy.verifyAndClickElement("#investigation", "Investigation No. 1");
   }
 
   clickInvestigationCheckbox() {
-    cy.log("Clicking Investigation Checkbox");
-    this.elements
-      .investigationCheckbox()
-      .should("be.visible")
-      .should("be.enabled")
-      .click();
+    cy.get("#investigation-checkbox").click();
   }
 
   selectInvestigationOption(options: string[]) {
-    cy.log("Selecting Investigation Options:", options);
-
-    this.elements
-      .investigationSelect()
-      .should("exist")
-      .should("be.visible")
-      .click();
-
-    options.forEach((option) => {
-      cy.get("[role='option']")
-        .contains(option)
-        .should("be.visible")
-        .then(($el) => {
-          if ($el.length > 0) {
-            cy.wrap($el).click();
-          } else {
-            throw new Error(`Option "${option}" not found in dropdown`);
-          }
-        });
-    });
-
-    cy.get("body").click(0, 0);
-    cy.wait(500);
+    cy.clickAndMultiSelectOption("#investigation-select", options);
   }
 
   clickLogLabResults() {
-    cy.log("Clicking Log Lab Results");
-
-    this.elements.logLabResults().scrollIntoView().should("be.visible");
-
-    cy.wait(1000);
-
-    this.elements
-      .logLabResults()
-      .contains("Log Lab Results")
-      .should("be.visible")
-      .should("be.enabled")
-      .click();
-
-    cy.wait(2000);
+    cy.verifyAndClickElement("#log-lab-results", "Log Lab Results");
   }
 
   selectInvestigationFrequency(frequency: string) {
-    cy.log(`Selecting Investigation Frequency: ${frequency}`);
-
-    this.elements.investigationFrequency().should("be.visible").click();
-
+    cy.get("#investigation-frequency").click();
     cy.contains("button", frequency).should("be.visible").click();
-
-    cy.wait(500);
-  }
-
-  verifyElementPresent(selector: string, timeout = 10000) {
-    return cy.get(selector, { timeout }).should("exist").should("be.visible");
-  }
-
-  waitForLoading() {
-    cy.get("#loading-indicator", { timeout: 10000 }).should("not.exist");
   }
 }
-
 export default PatientInvestigation;
