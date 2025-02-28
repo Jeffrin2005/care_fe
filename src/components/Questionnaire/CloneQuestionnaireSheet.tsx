f693ab7c673049270cc70a7bef38915b3549273e
Jeffrin2005 <jeffrinjojo1@gmail.com>
2025-02-28 21:26:18 +0530

    used radix-popover

--- src/components/Questionnaire/CloneQuestionnaireSheet.tsx
+++ src/components/Questionnaire/CloneQuestionnaireSheet.tsx
@@ -184,7 +184,10 @@ export default function CloneQuestionnaireSheet({
                   <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                 </Button>
               </PopoverTrigger>
-              <PopoverContent className="w-full p-0" align="start">
+              <PopoverContent
+                className="p-0 w-[var(--radix-popover-trigger-width)]"
+                align="start"
+              >
                 <Command>
                   <CommandInput
                     placeholder={t("search_organizations")}
